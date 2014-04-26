3f93e13d4ff043651dce8547b4a1d014eaa7abca
Bhagath <bhagath@bhagath-Inspiron-3521.(none)>
2014-04-26 11:03:18 +0530

    turbolinkchange

--- config/application.rb
+++ config/application.rb
@@ -30,10 +30,9 @@ module WebApp
 
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
-
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
-
+    config.assets.initialize_on_precompile = false
     # Use SQL instead of Active Record's schema dumper when creating the database.
     # This is necessary if your schema can't be completely dumped by the schema dumper,
     # like if you have constraints or database-specific column types
